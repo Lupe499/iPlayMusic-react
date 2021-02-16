c8ddb81067083b77813ee5f353a818f3b22af920
Lucas Pedersen <lucas.a.pedersen@gmail.com>
2021-02-16 12:14:07 +0100

    pre commit testing

--- src/pages/Featured.test.js
+++ src/pages/Featured.test.js
@@ -54,7 +54,7 @@ describe("Featured Page", function () {
 		});
 
 		await waitFor(function () {
-			var text = screen.getByText(/brain food/i);
+			var text = screen.getByText(/akfahfkasjh/i);
 			expect(text).toBeInTheDocument();
 		});
 	});
